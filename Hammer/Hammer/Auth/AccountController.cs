0b38ebf6f0309505e87efbe22d0c6b704c10d6f6
Ivan <ivan1501@gmail.com>
2020-01-31 10:23:36 +0100

    Auth in progress. Code under comments, fixing dependeny errors and configuration

---
+++ Hammer/Hammer/Auth/AccountController.cs
@@ -0,0 +1,46 @@
+﻿//using EntityWorker.Core.Helper;
+//using Hammer.Controllers;
+//using Hammer.Models;
+//using Hammer.Modules;
+//using System.Web.Mvc;
+
+//namespace IProduct.Controllers
+//{
+//    public class AccountController : SharedController
+//    {
+//        [AllowAnonymous]
+//        public ActionResult Index(GenericView<User> user, string type = "")
+//        {
+//            if (Request.IsAuthenticated)
+//                return Redirect("~/Home");
+//            else if (type.ConvertValue<SignInApplication?>().HasValue)
+//            {
+//                using (var manager = new UserManager())
+//                {
+//                    if (!Request.IsAuthenticated)
+//                    {
+//                        manager.SignIn(type.ConvertValue<SignInApplication>(), user);
+//                    }
+//                }
+
+//                if (type.ConvertValue<SignInApplication>() == SignInApplication.Cookie &&
+//                    !Request.IsAuthenticated)
+//                    return View(user.Error("Email or Password could not be found."));
+//            }
+//            return View(user ?? new GenericView<User>());
+//        }
+
+//        #region Facebook
+//        // we may need to add some changes here later as if now, the Facebook provider
+//        // take care of the login
+//        [AllowAnonymous]
+//        public ActionResult Facebook(string error)
+//        {
+//            if (Request.IsAuthenticated)
+//                return Redirect("~/Home");
+
+//            return Redirect("Index");
+//        }
+//        #endregion
+//    }
+//}
---
+++ Hammer/Hammer/Auth/AuthorizeAttribute.cs
@@ -0,0 +1,40 @@
+﻿//using System.Linq;
+//using System.Security.Claims;
+//using System.Web;
+//using System.Web.Mvc;
+
+//namespace Hammer.Auth
+//{
+//    public class Authorize : AuthorizeAttribute
+//    {
+//        public Authorize()
+//        {
+//            base.Roles = IProduct.Modules.Roles.Customers.ToString();
+//        }
+
+//        public Authorize(params IProduct.Modules.Roles[] roles)
+//        {
+//            if (roles != null && roles.Any())
+//                base.Roles = string.Join(",", roles.Select(x => x.ToString()));
+//            else
+//                base.Roles = IProduct.Modules.Roles.Customers.ToString();
+//        }
+
+//        protected override void HandleUnauthorizedRequest(AuthorizationContext ctx)
+//        {
+//            if (!ctx.HttpContext.User.Identity.IsAuthenticated)
+//                base.HandleUnauthorizedRequest(ctx);
+//            else
+//            {
+//                var role = HttpContext.Current.GetOwinContext().Authentication.User.Claims
+//                           .FirstOrDefault(x => x.Type == "role" || x.Type == ClaimTypes.Role)?.Value;
+//                // Role check IsAuthenticated
+//                if (!(role == IProduct.Modules.Roles.Administrator.ToString() || Roles.Contains(role)))
+//                {
+//                    ctx.Result = new ViewResult { ViewName = "Unauthorized" };
+//                    ctx.HttpContext.Response.StatusCode = 403;
+//                }
+//            }
+//        }
+//    }
+//}
